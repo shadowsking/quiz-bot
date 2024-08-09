22399a434d7ba54b2d0dfc7209e98086cd7e5ec6
shadowsking <shadowsking1295@gmail.com>
2024-08-09 03:51:39 +0300

    Added states

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,13 +7,14 @@ import dotenv
 import quiz_parser
 import redis
 import telegram
-from telegram import Update
+from telegram import Update, ReplyKeyboardRemove
 from telegram.ext import (
     Updater,
     CommandHandler,
     MessageHandler,
     Filters,
     CallbackContext,
+    ConversationHandler,
 )
 
 # Enable logging
@@ -23,42 +24,61 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+(NEW_QUESTION, ATTEMPT, SURRENDER) = range(3)
 
-def start(update: Update, context: CallbackContext) -> None:
+
+def start(update: Update, context: CallbackContext) -> int:
     update.message.reply_text(
         text="Привет! Я бот для викторин!", reply_markup=get_keyboard_markup()
     )
+    return NEW_QUESTION
+
+
+def stop(update: Update, context: CallbackContext) -> int:
+    context.dispatcher.redis.delete(update.message.from_user.id)
+    update.message.reply_text("Пока", reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Help!")
 
 
-def reply_text(update: Update, context: CallbackContext) -> None:
-    text = update.message.text
-    if update.message.text == "Новый вопрос":
-        questions = context.dispatcher.questions
-        random_question_index = randint(0, len(questions) - 1)
-        text = context.dispatcher.questions[random_question_index]["question"]
-        context.dispatcher.redis.set(
-            update.message.from_user.id, int(random_question_index)
-        )
-        update.message.reply_text(text)
-        return
-
+def get_answer(update: Update, context: CallbackContext) -> str | None:
     question_index = context.dispatcher.redis.get(update.message.from_user.id)
-    if not question_index:
-        return
+    if question_index:
+        return context.dispatcher.questions[int(question_index)]["answer"]
 
-    answer = context.dispatcher.questions[int(question_index)]["answer"]
-    if update.message.text == "Сдаться":
-        text = f"Вот тебе правильный ответ: {answer}"
-    elif text == answer:  # todo fix
-        text = "Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»"
-    else:
-        text = "Неправильно… Попробуешь ещё раз?"
+    return None
 
+
+def surrender(update: Update, context: CallbackContext) -> int:
+    answer = get_answer(update, context)
+    if answer:
+        update.message.reply_text(f"Вот тебе правильный ответ: {answer}")
+
+    return ConversationHandler.END
+
+
+def handle_new_question_request(update: Update, context: CallbackContext) -> None | int:
+    questions = context.dispatcher.questions
+    random_question_index = randint(0, len(questions) - 1)
+    text = context.dispatcher.questions[random_question_index]["question"]
+    context.dispatcher.redis.set(
+        update.message.from_user.id, int(random_question_index)
+    )
     update.message.reply_text(text)
+
+    return ATTEMPT
+
+
+def handle_solution_attempt(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+    if text != get_answer(update, context):  # todo
+        update.message.reply_text("Неправильно… Попробуешь ещё раз?")
+        return ATTEMPT
+
+    return NEW_QUESTION
 
 
 def get_keyboard_markup():
@@ -68,7 +88,7 @@ def get_keyboard_markup():
 def main() -> None:
     dotenv.load_dotenv()
 
-    parser = argparse.ArgumentParser(description="Create new intents from json file")
+    parser = argparse.ArgumentParser(description="Quiz")
     parser.add_argument(
         "-f",
         "--file",
@@ -81,15 +101,30 @@ def main() -> None:
     updater = Updater(os.getenv("TELEGRAM_TOKEN"))
 
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, reply_text))
     dispatcher.redis = redis.Redis(
         host=os.environ.get("REDIS_HOST", "localhost"),
         port=os.environ.get("REDIS_PORT", 6379),
         db=0,
     )
     dispatcher.questions = quiz_parser.get_questions_from_json(args.file)
+
+    new_question_handler = MessageHandler(
+        Filters.text & Filters.regex("^Новый вопрос$"), handle_new_question_request
+    )
+    conversation_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start), new_question_handler],
+        states={
+            NEW_QUESTION: [new_question_handler],
+            ATTEMPT: [
+                MessageHandler(Filters.text & Filters.regex("^Сдаться$"), surrender),
+                MessageHandler(
+                    Filters.text & ~Filters.command, handle_solution_attempt
+                ),
+            ],
+        },
+        fallbacks=[CommandHandler("stop", stop)],
+    )
+    dispatcher.add_handler(conversation_handler)
 
     updater.start_polling()
 
